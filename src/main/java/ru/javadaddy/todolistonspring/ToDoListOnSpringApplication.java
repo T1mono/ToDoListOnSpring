4fbe0d11aa499b7766ffb47d4417defe9d4e065c
T1mono <167001770+T1mono@users.noreply.github.com>
2025-08-14 14:35:57 +0500

    git init

---
+++ src/main/java/ru/javadaddy/todolistonspring/ToDoListOnSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.javadaddy.todolistonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListOnSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListOnSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javadaddy/todolistonspring/ToDoListOnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javadaddy.todolistonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListOnSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
