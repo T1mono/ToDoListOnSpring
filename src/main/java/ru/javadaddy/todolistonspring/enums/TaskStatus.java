899bf53f2d589c27b6418ecf58a2b37efeadaf72
T1mono <167001770+T1mono@users.noreply.github.com>
2025-08-14 16:49:35 +0500

    Created enums and database

---
+++ src/main/java/ru/javadaddy/todolistonspring/enums/TaskStatus.java
@@ -0,0 +1,18 @@
+package ru.javadaddy.todolistonspring.enums;
+
+public enum TaskStatus {
+    TODO("К выполнению"),
+    IN_PROGRESS("В процессе"),
+
+    DONE("Завершено");
+
+    private final String description;
+
+    TaskStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
