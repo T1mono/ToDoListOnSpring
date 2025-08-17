3058393f8d9a9fa508e37f255f85091f8498b4a0
T1mono <167001770+T1mono@users.noreply.github.com>
2025-08-17 17:30:43 +0500

    Добавил @RestControllerAdvice

---
+++ src/main/java/ru/javadaddy/todolistonspring/exception/ApiError.java
@@ -0,0 +1,14 @@
+package ru.javadaddy.todolistonspring.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ApiError {
+    private String message;
+    private int status;
+    private LocalDateTime timestamp;
+}
--- src/main/java/ru/javadaddy/todolistonspring/exception/TaskNotFoundException.java
+++ src/main/java/ru/javadaddy/todolistonspring/exception/TaskNotFoundException.java
@@ -1,15 +1,9 @@
 package ru.javadaddy.todolistonspring.exception;
 
-public class TaskNotFoundException extends RuntimeException{
+public class TaskNotFoundException extends RuntimeException {
 
     //Конструктор с собщением об ошибке
     public TaskNotFoundException(String message) {
         super(message);
-    }
-
-    //Конструктор с сообщением и причиной(Throwable)
-
-    public TaskNotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
