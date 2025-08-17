fc50353ff088774d1025afff3cab7b93750025d0
T1mono <167001770+T1mono@users.noreply.github.com>
2025-08-17 18:53:36 +0500

    Hot fix

--- src/main/java/ru/javadaddy/todolistonspring/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/javadaddy/todolistonspring/exception/GlobalExceptionHandler.java
@@ -1,34 +1,36 @@
-//package ru.javadaddy.todolistonspring.exception;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//import java.time.LocalDateTime;
-//
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(TaskNotFoundException.class)
-//    public ResponseEntity<ApiError> handleTaskNotFound(TaskNotFoundException ex) {
-//        ApiError error = new ApiError(
-//                ex.getMessage(),
-//                HttpStatus.NOT_FOUND.value(),
-//                LocalDateTime.now()
-//        );
-//
-//        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-//    }
-//
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    public ResponseEntity<ApiError> handleIllegalArgumentException(IllegalArgumentException ex) {
-//        ApiError error = new ApiError(
-//                ex.getMessage(),
-//                HttpStatus.BAD_REQUEST.value(),
-//                LocalDateTime.now()
-//        );
-//
-//        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-//    }
-//}
+package ru.javadaddy.todolistonspring.exception;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice(basePackages = {"ru.javadaddy.todolistonspring.controller"})
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ApiError> handleTaskNotFound(TaskNotFoundException ex) {
+        ApiError error = new ApiError(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiError> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ApiError error = new ApiError(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
